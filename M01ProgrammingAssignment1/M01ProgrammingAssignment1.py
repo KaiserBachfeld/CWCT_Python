3638ab728aac4c762e9cdcd07f4acc84f759bd44
KBach <jamesbachelder@gmail.com>
2021-06-19 20:40:20 -0500

    Delete M01ProgrammingAssignment1 directory

--- M01ProgrammingAssignment1/M01ProgrammingAssignment1.py
+++
@@ -1,9 +0,0 @@
-#################################################################
-# James Bachelder                                               #
-# CWCT - Python Essentials                                      #
-# M01 Programming Assignment 1                                  #
-# 12 June 2021                                                  #
-# Purpose: This code will print 4 lines of personal information #
-#################################################################
-
-
